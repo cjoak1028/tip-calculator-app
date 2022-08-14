2aff80b7908da6a7161b811eaff38a336c909e6d
David Kim <cjoak1028@gmail.com>
2022-08-13 22:10:13 -0400

    added reset styles, variables, and logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <div className="app">
+      <img src="./images/logo.svg" alt="splitter logo" />
+    </div>
+  );
+};
 
 export default App;
